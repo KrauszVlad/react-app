e845832b8afe260795ea6b7aa3a458d37767a4d0
KrauszVlad <tibiaremere@gmail.com>
2024-07-06 12:06:14 +0300

    Fourh  commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 function Home() {
-  return  <div>Hello World</div>;
+  return  <div className="home"> 
+  <div className="headerContainer"> 
+    <h1> Romanian Steam </h1>
+    <p> Our games are a steal </p>
+    <Link to="/menu">
+    <button> Comanda Acuma</button>
+    </Link>
+      </div>
+    
+    </div>;
 }
 
 export default Home
