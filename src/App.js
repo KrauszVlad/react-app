63d5322920439aca20884fa3102871ffa68d183a
KrauszVlad <tibiaremere@gmail.com>
2024-07-09 20:07:31 +0300

    Last Stop

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Menu from "./pages/Menu";
+import Contact from "./pages/Contact";
+import About from './pages/about';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./styles/Footer.css";
 function App() {
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Routes>
         <Route path='/' exact element={<Home />} />
+        <Route path='/menu' exact element={<Menu />} />
+        <Route path="/about" exact component={About} />
+        <Route path='/contact' exact element={<Contact />} />
         </Routes>
       <Footer />
       </Router>
---
+++ src/helpers/MenuLIst.js
@@ -0,0 +1,42 @@
+import Outlast from '../assets/outlast.jpg'
+import FarCry from '../assets/farcry.jpg';
+import NeedForSpeed from '../assets/needforspeed.jpg';
+import SoloLeveling from "../assets/sololeveling.jpg";
+import Warcraft  from "../assets/warcraft.jpg";
+import CallOfDuty  from "../assets/callofduty.jpg";
+
+
+
+export const MenuLIst = [
+    {
+      name: "Outlast",
+      image: Outlast,
+      price: 59.99,
+    },
+    {
+      name: "Farcry",
+      image: FarCry,
+      price: 59.99,
+    },
+    {
+      name: "NeedForSpeed",
+      image: NeedForSpeed,
+      price: 45.00,
+    },
+    {
+      name: "SoloLeveling",
+      image: SoloLeveling,
+      price: 48.99,
+    },
+    {
+      name: "Warcraft",
+      image: Warcraft,
+      price: 89.99,
+    },
+    {
+      name: "CallOfDuty",
+      image: CallOfDuty,
+      price: 500.00,
+    },
+  ];
+  export default MenuLIst;
